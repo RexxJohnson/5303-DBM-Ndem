19580b9384c38c10de96eea9a590252767724e6b
Emmanuel Ndem <emmanuelndem@MacBook-Pro.local>
2019-12-13 10:45:55 -0600

    modified

---
+++ A_09/converCsvToJson.py
@@ -0,0 +1,16 @@
+import csv, json
+
+csvFilePath = '/Users/emmanuelndem/Desktop/data.csv'
+jsonFilePath = '/Users/emmanuelndem/Desktop/data.json'
+
+#read
+data = {}
+with open(csvFilePath) as csvFile:
+    csvReader = csv.DictReader(csvFile)
+    for rows in csvReader:
+        id = rows['id']
+        data[id] = rows
+
+#create json
+with open(jsonFilePath, 'w') as jsonFile:
+    jsonFile.write(json.dumps(data, indent=4))
